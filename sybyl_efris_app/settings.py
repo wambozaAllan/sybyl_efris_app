0a8e5e9aa76d46fcde1674e9cfcde264f8faf31f
wambozaAllan <w.allan489@gmail.com>
2020-12-15 14:44:40 +0300

    database

--- sybyl_efris_app/settings.py
+++ sybyl_efris_app/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'sybyl_efris_app.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'sybyl_efris_app_db',
+        'USER': 'postgres',
+        'PASSWORD': 'password@123',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
